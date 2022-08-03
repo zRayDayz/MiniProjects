cb8f6b3fe1bc06dca9af0666813dfeae67ef9002
RayDay <70919071+zRayDayz@users.noreply.github.com>
2022-08-03 22:42:44 +0300

    Add files via upload

---
+++ GameTranslator/GameTranslator/View/TranslationForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace GameTranslator
+{
+    partial class TranslationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.translationRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // translationRichTextBox
+            // 
+            this.translationRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.translationRichTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(6)))), ((int)(((byte)(13)))));
+            this.translationRichTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.translationRichTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.translationRichTextBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.translationRichTextBox.Location = new System.Drawing.Point(0, -1);
+            this.translationRichTextBox.Name = "translationRichTextBox";
+            this.translationRichTextBox.ReadOnly = true;
+            this.translationRichTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.translationRichTextBox.Size = new System.Drawing.Size(914, 525);
+            this.translationRichTextBox.TabIndex = 1;
+            this.translationRichTextBox.Text = "Initial text";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.translationRichTextBox);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(916, 525);
+            this.panel1.TabIndex = 2;
+            // 
+            // TranslationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(940, 549);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Name = "TranslationForm";
+            this.Text = "TranslationForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.TranslationForm_FormClosed);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox translationRichTextBox;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
