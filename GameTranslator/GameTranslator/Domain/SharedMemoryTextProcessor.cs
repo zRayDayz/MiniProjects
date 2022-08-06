9658d6355be40e2c66d46a73c5f1e03ecb612505
RayDay <70919071+zRayDayz@users.noreply.github.com>
2022-08-06 20:08:05 +0300

    Add files via upload

---
+++ GameTranslator/GameTranslator/Domain/SharedMemoryTextProcessor.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameTranslator
+{
+    class SharedMemoryTextProcessor
+    {
+        string sharedMemoryName = "SharedMemoryForSubtitlesStrs";   // в дальнейшем константу можно вынести в UI для возможности выбора имени пользователю
+        int initSharedMemorySize = 32768;       // не играет роли, если Shared memory открывается, а не создается (в моем случае именно так и происходит, т.к. создает Shared memory Cheat Engine)
+        long openedSharedMemorySize;            // если Shared memory открывается, сюда положится ее реальный размер (указанный в Cheat Engine)
+        MemoryMappedFile memoryMappedFile;
+        MemoryMappedViewAccessor accessor;
+
+        BackgroundWorker backgroundWorker;
+        BaseTranslatorProcessor translatorBootstrapper;
+
+        byte[] tempBytesOfStr = new byte[64];
+
+        public SharedMemoryTextProcessor()
+        {            
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.WorkerSupportsCancellation = true;
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
+            backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+        }
+
+        public bool StartTextProcessor(BaseTranslatorProcessor translatorBootstrapper)
+        {
+            if (backgroundWorker.IsBusy)
+            {
+                MessageBox.Show("Runtime Translator в процессе завершения работы, требуется дождаться его завершения перед новым запуском.", "Error.", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+            memoryMappedFile = MemoryMappedFile.CreateOrOpen(sharedMemoryName, initSharedMemorySize);
+            accessor = memoryMappedFile.CreateViewAccessor();
+            openedSharedMemorySize = accessor.Capacity;
+
+            this.translatorBootstrapper = translatorBootstrapper;
+            backgroundWorker.RunWorkerAsync();
+            return true;
+        }
+
+        public bool StopTextProcessor()
+        {
+            if (backgroundWorker.IsBusy)
+            {
+                backgroundWorker.CancelAsync();
+                return true;
+            }
+            return false;
+        }
+
+        void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            memoryMappedFile.Dispose();
+            accessor.Dispose();
+
+            if (e.Error != null)
+            {
+                MessageBox.Show("Произошла ошибка в Background worker-е: " + Environment.NewLine + e.Error.ToString(), "Error.", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+        }
+
+        
+        void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.UserState is List<string> listOfLines)
+            {              
+                translatorBootstrapper.TranslateAndAppendTextInTranslationWindow(listOfLines);
+                ListOfStringPool.Instance.ReturnObject(listOfLines, true);
+            }
+            
+        }
+
+        readonly int mainDataInfoStructSize = 16;
+        struct MainSharedMemoryDataInfo
+        {
+            public int lockVar;
+            public int counter;
+            public int maxCounter;
+            public int reserveInt32;
+        }
+        void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {            
+
+            int previousIndex = 0;
+            int maxCountOfPointers = -1;
+            List<string> listOfLines = new List<string>(32);
+
+            while (true)
+            {
+                if (backgroundWorker.CancellationPending) return;
+
+                accessor.Read(0, out MainSharedMemoryDataInfo mainInfo);
+
+                if (mainInfo.lockVar == 1)
+                {
+                    Thread.Sleep(1);
+                    continue;
+                }
+
+                int counter = mainInfo.counter;
+                if (counter == 0 || counter == previousIndex)
+                {
+                    Thread.Sleep(1);
+                    continue;
+                }
+
+                long pointersArrBeginningOffset = mainDataInfoStructSize;
+
+                int indexOfLastAddedStringToRead = counter - 1;
+
+                if (indexOfLastAddedStringToRead < previousIndex)
+                {
+                    if (maxCountOfPointers == -1)
+                    {
+                        accessor.Read(8, out int tempMaxCountOfPointers);
+                        maxCountOfPointers = tempMaxCountOfPointers;
+                    }
+
+                    ReadAllNewStringSinceLastRead(maxCountOfPointers, ref previousIndex, pointersArrBeginningOffset, ref listOfLines);
+                    previousIndex = 0;
+                }
+
+                ReadAllNewStringSinceLastRead(counter, ref previousIndex, pointersArrBeginningOffset, ref listOfLines);
+
+                var tempListOfLines = ListOfStringPool.Instance.GetObject();
+                tempListOfLines.AddRange(listOfLines);
+                backgroundWorker.ReportProgress(0, tempListOfLines);
+                listOfLines.Clear();
+            }
+
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void ReadAllNewStringSinceLastRead(int count, ref int startIndex, long pointersArrBeginningOffset, ref List<string> listOfLines)
+        {
+            while (startIndex < count)
+            {
+                long pointersArrOffset = pointersArrBeginningOffset + startIndex * 8;
+                accessor.Read(pointersArrOffset, out long offsetOfStringLength);
+
+                if (offsetOfStringLength == 0)
+                {
+                    startIndex = 0;
+                    return;
+                }
+
+                string finalString = ReadLineByOffset(offsetOfStringLength);
+                listOfLines.Add(finalString);
+
+                startIndex++;
+            }
+
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        string ReadLineByOffset(long offsetOfStringLength)
+        {
+            accessor.Read(offsetOfStringLength, out int strLength);
+
+            // Первыми 4 байтами в строке являются ее размер
+            if(offsetOfStringLength + 4 + strLength > openedSharedMemorySize)
+            {
+                throw new BadReadedStringLengthException(offsetOfStringLength, strLength);
+            }
+
+            if (tempBytesOfStr.Length < strLength) Array.Resize(ref tempBytesOfStr, strLength);
+            accessor.ReadArray(offsetOfStringLength + 4, tempBytesOfStr, 0, strLength);
+            string finalStr = Encoding.Unicode.GetString(tempBytesOfStr, 0, strLength);
+
+            return finalStr;
+        }
+
+    }
+
+    class BadReadedStringLengthException : Exception
+    {
+        const string ERROR_MESSAGE = "Кастомный OutOfMemoryException. Прочитанная длина строки оказалась слишком большой. Вероятно ошибка синхронизации.";
+        public long OffsetOfStringLength { get; private set; }
+        public int ReadedStrLength { get; private set; }
+        public BadReadedStringLengthException(long offsetOfStringLength, int readedStrLength) : base(ERROR_MESSAGE)
+        {
+            OffsetOfStringLength = offsetOfStringLength;
+            ReadedStrLength = readedStrLength;
+        }
+
+        public override string ToString()
+        {
+            string mainParams = "Параметры исключения: " + Environment.NewLine + "Оффсет (от начала Shared memory) по которому читалась длина строки: " + OffsetOfStringLength + Environment.NewLine +
+                "Прочитанная (ошибочная) длина строки: " + ReadedStrLength + Environment.NewLine;
+            return mainParams + base.ToString();
+        }
+
+    }
+
+
+}
---
+++ GameTranslator/GameTranslator/Domain/TranslatorProcessor.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameTranslator
+{
+    enum TranslatorProcessorState
+    {
+        BeginningState,
+        TranslationDataInitialized,
+        TranslatorIsRunning
+    }
+
+    public abstract class BaseTranslatorProcessor
+    {
+        public abstract bool GenerateTranslationData(string pathForOriginalStringsFile, string pathForTranslatedStringsFile);
+        public abstract bool RunTranslator();
+        public abstract bool StopTranslator();
+        public abstract void TranslateAndAppendTextInTranslationWindow(List<string> listOfLines);
+        public abstract void OnTranslationFormClosed(Form form, FormClosedEventArgs e);
+    }
+
+    public class TranslatorProcessor : BaseTranslatorProcessor
+    {
+        BaseViewController baseViewController;
+        BaseTranslationFileProcessor baseTranslationFileProcessor;
+        SharedMemoryTextProcessor textProcessor;
+
+        Encoding targetEncoding;
+        TranslatorProcessorState currentState;
+        IntPtr gameWindowPtr;
+
+        public TranslatorProcessor(BaseViewController baseViewController, BaseTranslationFileProcessor baseTranslationFileProcessor)
+        {
+            this.baseViewController = baseViewController;
+            baseViewController.Initialization(this);
+            this.baseTranslationFileProcessor = baseTranslationFileProcessor;
+            textProcessor = new SharedMemoryTextProcessor();
+
+            targetEncoding = Encoding.Unicode;
+            currentState = TranslatorProcessorState.BeginningState;
+        }
+
+        public override void OnTranslationFormClosed(Form form, FormClosedEventArgs e)
+        {
+            StopTranslator();
+        }
+
+        public override bool GenerateTranslationData(string pathForOriginalStringsFile, string pathForTranslatedStringsFile)
+        {
+            bool result = baseTranslationFileProcessor.GenerateTranslationData(pathForOriginalStringsFile, pathForTranslatedStringsFile, targetEncoding);
+            if(result == true)
+            {
+                currentState = TranslatorProcessorState.TranslationDataInitialized;
+            }
+            return result;
+        }
+
+        public override bool RunTranslator()
+        {
+            if (currentState < TranslatorProcessorState.TranslationDataInitialized)
+            {
+                MessageBox.Show("Требуется сначала проинициализировать данные для перевода.", "Error.", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+            bool result = textProcessor.StartTextProcessor(this);
+            if (result)
+            {
+                currentState = TranslatorProcessorState.TranslatorIsRunning;
+                return true;
+            }
+            return false;
+        }
+
+        public override bool StopTranslator()
+        {
+            bool result = textProcessor.StopTextProcessor();
+            if (result)
+            {
+                currentState = TranslatorProcessorState.TranslationDataInitialized;
+                baseViewController.UpdateTranslatorStatus(false);
+                return true;
+            }
+            return false;
+        }
+
+        public override void TranslateAndAppendTextInTranslationWindow(List<string> listOfLines)
+        {
+            for (int i = 0; i < listOfLines.Count; i++)
+            {
+                string text = listOfLines[i];
+                if (text.Length == 1)
+                {
+                    var outStr = text + "\t\t- Перевода нет, т.к. строка в один символ";
+                    Console.WriteLine(outStr);
+                }
+                else if (baseTranslationFileProcessor.TryGetTranslation(text, out string translatedStr))
+                {
+                    var outStr = "=> " + text + "\n     " + translatedStr;
+                    baseViewController.PrintNewLine(outStr);
+                }
+                else
+                {
+                    string outStr;
+                    if (text.Length < 100)
+                    {
+                        outStr = $"{text} \t- перевод не был найден.";                        
+                    }
+                    else
+                    {
+                        outStr = text.Substring(0, 100);
+                        outStr = $"{outStr} \t- перевод не был найден. Строка слишком большая поэтому была обрезана.";
+                    }
+                    Console.WriteLine(outStr);
+                }
+            }           
+        }
+
+    }
+
+}
